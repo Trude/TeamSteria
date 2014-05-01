e960db26c392b7696d5adac45bbcd3daa6e19e52
Trude <trude.martinsen@gmail.com>
2014-05-01 19:16:15 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TeamSteria::Application.config.secret_key_base = 'cfbbcb97d6efbc4adb41dc0f44ddc1f0b368bc73ae494d6260fdb3a238927c2844130820ae14fb5c16171a8d33f82dcf90a8cc0c0ec6851c90cb28c9a8200d69'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TeamSteria::Application.config.session_store :cookie_store, key: '_TeamSteria_session'
